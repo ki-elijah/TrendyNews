85f152d72268fad83b8644865cda17e5b6151ad5
elabs <elijahknsubuga@gmail.com>
2021-10-06 15:37:32 +0300

    made the seach for uganda

---
+++ app.py
@@ -0,0 +1,12 @@
+from GoogleNews import GoogleNews
+
+googlenews = GoogleNews()
+googlenews = GoogleNews(period = '7')
+googlenews.search('UG')
+result = googlenews.result()
+for x in result:
+    print("_"*50)
+    print("Title--", x['title'])
+    print("Date--", x['date'])
+    print("Description--", x['desc'])
+    print("Link--", x['link'])
